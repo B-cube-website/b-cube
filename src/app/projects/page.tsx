56923eb81eb41abf6271eef25148b841508315f9
Minjae Joe <ipszone0525@gmail.com>
2024-08-25 19:18:40 +0900

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,16 @@
-export default function Project () {
+import React from 'react';
+import PostPreviewBox from "@/components/postPreview/PostPreviewBox";
+import BlueButton from '@/components/button/BlueButton';
+import WhiteButton from '@/components/button/WhiteButton';
+
+const Project: React.FC = () => {
   return (
-    <main>
-      <h1>프로젝트</h1>
+    <main className="flex flex-col">
+      <h2>B-Cube의 다양한 활동들을 지금 확인해 보세요</h2>
+      <WhiteButton />
+      <PostPreviewBox year="2022" title="My Project" participants={['John', 'Jane']} />
     </main>
-  )
-}
+  );
+};
+
+export default Project;
