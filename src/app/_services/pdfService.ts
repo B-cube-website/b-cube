33b7c9fbaa28d847d0e993b0662e46a51162ba74
onehowon <flash0221@naver.com>
2024-09-01 21:04:58 +0900

    feat: 디자인톤 POST 기능 완료

--- src/app/_services/pdfService.ts
+++ src/app/_services/pdfService.ts
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-interface GenerateImageUrlOptions {
+interface GeneratePdfUrlOptions {
   bucketName: string;
   objectName: string;
 }
 
-const generateImageUrl = (options: GenerateImageUrlOptions): string => {
+const generatePdfUrl = (options: GeneratePdfUrlOptions): string => {
 
   const region: string = process.env.OCI_REGION ?? 'ap-chuncheon-1';
   const namespace: string = process.env.OCI_NAMESPACE ?? 'axsqxeygavsg';
@@ -15,9 +15,8 @@ const generateImageUrl = (options: GenerateImageUrlOptions): string => {
   const bucketName: string = options.bucketName;
   const objectName: string = options.objectName;
   
-  const url = `https://${namespace}.compat.objectstorage.${region}.oraclecloud.com/n/${options.bucketName}/b/${options.objectName}/o/`;
+  const url = `https://${namespace}.compat.objectstorage.${region}.oraclecloud.com/n/${namespace}/b/${bucketName}/o/${objectName}`;
   return url;
 };
 
-export { generateImageUrl };
-
+export { generatePdfUrl };
