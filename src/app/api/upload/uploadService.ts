d64e2b4fc9d2db558e28c5f1cbfaf5b37c742262
dongdongs <ehdgusdlwwkd@ajou.ac.kr>
2024-09-17 19:37:26 +0900

    build: 빌드시도

--- src/app/api/upload/uploadService.ts
+++ src/app/api/upload/uploadService.ts
@@ -1,44 +1,50 @@
-import { ObjectStorageClient } from 'oci-objectstorage';
-import path from 'path';
-import crypto from 'crypto';
-import { generateImageUrl } from '../../_services/imageService';
-import { Readable } from 'stream';
+import { ObjectStorageClient } from "oci-objectstorage";
+import path from "path";
+import crypto from "crypto";
+import { generateImageUrl } from "../../_services/imageService";
+import { Readable } from "stream";
 
-
-const BUCKET_NAME = process.env.OCI_BUCKET_NAME ?? '';
-const NAMESPACE = process.env.OCI_NAMESPACE ?? '';
+const BUCKET_NAME = process.env.OCI_BUCKET_NAME ?? "";
+const NAMESPACE = process.env.OCI_NAMESPACE ?? "";
 
 if (!BUCKET_NAME || !NAMESPACE) {
-  throw new Error('환경변수 NAMESPACE 또는 BUCKET_NAME이 정의되지 않았습니다.');
+  throw new Error("환경변수 NAMESPACE 또는 BUCKET_NAME이 정의되지 않았습니다.");
 }
 
-export async function uploadImage(fileBuffer: Buffer, fileName: string, objectStorageClient: ObjectStorageClient): Promise<{ uniqueId: string, imageUrl: string }> {
-  const ext = path.extname(fileName).toLowerCase() || '.png';
-  const uniqueId = crypto.randomBytes(16).toString('hex');
+export async function uploadImage(
+  fileBuffer: Buffer,
+  fileName: string,
+  objectStorageClient: ObjectStorageClient
+): Promise<{ uniqueId: string; imageUrl: string }> {
+  const ext = path.extname(fileName).toLowerCase() || ".png";
+  const uniqueId = crypto.randomBytes(16).toString("hex");
   const objectName = `image/${uniqueId}${ext}`;
 
   const fileStream = Readable.from(fileBuffer);
 
   if (NAMESPACE && BUCKET_NAME) {
-    return objectStorageClient.putObject({
-      namespaceName: NAMESPACE,
-      bucketName: BUCKET_NAME,
-      objectName: objectName,
-      putObjectBody: fileStream,
-    })
-    .then(() => {
-      console.log(`파일 ${objectName} 업로드 완료`);
-      const imageUrl = generateImageUrl({
+    return objectStorageClient
+      .putObject({
+        namespaceName: NAMESPACE,
         bucketName: BUCKET_NAME,
         objectName: objectName,
+        putObjectBody: fileStream,
+      })
+      .then(() => {
+        console.log(`파일 ${objectName} 업로드 완료`);
+        const imageUrl = generateImageUrl({
+          bucketName: BUCKET_NAME,
+          objectName: objectName,
+        });
+        return { uniqueId, imageUrl };
+      })
+      .catch((error) => {
+        console.error(`업로드 실패 ${objectName}:`, error);
+        throw error;
       });
-      return { uniqueId, imageUrl };
-    })
-    .catch((error) => {
-      console.error(`업로드 실패 ${objectName}:`, error);
-      throw error;
-    });
   } else {
-    throw new Error('환경변수 NAMESPACE 또는 BUCKET_NAME이 정의되지 않았습니다.');
+    throw new Error(
+      "환경변수 NAMESPACE 또는 BUCKET_NAME이 정의되지 않았습니다."
+    );
   }
 }
