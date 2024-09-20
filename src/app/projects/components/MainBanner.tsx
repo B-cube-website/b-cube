7b4e57bdbef9c4d1f87d9813a9d940b1494a4e45
Minjae Joe <ipszone0525@gmail.com>
2024-09-20 21:02:52 +0900

    파일과 전구 개선

--- src/app/projects/components/MainBanner.tsx
+++ src/app/projects/components/MainBanner.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 import Image from "next/image";
-import bulb from "@/../public/bulb.svg";
-import file_image from "@/../public/file_image.svg";
+import file_image from "@/../public/file-bulb.png";
 
 const MainBanner = () => {
   return (
-    <div className="relative overflow-hidden px-4 sm:px-6 lg:px-8 max-w-screen-2xl mx-auto h-[700px]">
-      <div className="w-[733.58px] h-[733.58px] absolute left-[756.58px] top-[82.11px] object-cover">
-        <Image src={file_image} alt="file_image" />
-        <Image src={bulb} alt="bulb" />
+    <div className="relative overflow-hidden px-4 sm:px-6 lg:px-8 max-w-screen-2xl mx-auto h-[500px] sm:h-[700px]">
+      <div className="w-[733.58px] h-[733.58px] absolute right-[0px] top-[82.11px]">
+        <Image src={file_image} alt="file_image" className="blur-sm" />
       </div>
-      <div className="flex flex-col justify-start items-start absolute left-[98px] top-[117px] gap-6">
-        <h1
-          className="text-5xl font-bold text-left text-white leading-normal"
-          style={{ maxWidth: "38rem" }}
-        >
+      <div className="flex flex-col justify-start items-start absolute left-[20px] sm:left-[98px] top-[50px] sm:top-[117px] gap-4 sm:gap-6">
+        <h1 className="text-2xl sm:text-5xl font-bold text-left text-white leading-normal max-w-xs sm:max-w-xl">
           B-CUBE의 다양한 활동들을 지금 확인해 보세요
         </h1>
       </div>
